fd74ab486b9d0ec926c37080b2e202eeb0126a4c
achrxf <achrxf@hotmail.com>
2023-10-15 00:15:11 +0100

    specifier

---
+++ specifier.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * specifier - checks for the specifiers and hundles them.
+ * @format: the string
+ * @print: the string list
+ * Return: cont
+ */
+int specifier(const char *format, va_list print)
+{
+	if (*format == 'c')
+	{
+		return (character(print));
+	}
+	else if (*format == 's')
+	{
+		return (string(print));
+	}
+	else if (*format == '%')
+	{
+		return (percent());
+	}
+	else if (*format == 'd' || *format == 'i')
+	{
+		return (integer(print));
+	}
+	return (0);
+}
