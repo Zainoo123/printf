41a02867bb98444074106dd25b64b2ac7e3cff7a
Zainoo123 <zaisarhane123@gmail.com>
2023-10-14 15:32:58 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int character(va_list print);
+int string(va_list print);
+int percent();
+int integer(va_list print);
+int number_hundler(int i);
+int specifier(const char *format, va_list print);
+
+#endif
