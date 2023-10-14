f260c25b118b880325ddbf47074d4fb24b3a94b0
Zainoo123 <zaisarhane123@gmail.com>
2023-10-14 16:31:14 -0700

    _printf

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _printf - a printf like function
+ * @format: the string
+ * Return: cont
+ */
+int _printf(const char *format, ...)
+{
+	int cont = 0;
+
+	va_list print;
+
+	va_start(print, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			cont += specifier(format, print);
+			if (*format && *format != 'c' && *format != 's' && *format != '%' && *format != 'd' && *format != 'i')
+		{
+			_putchar('%');
+			_putchar(*format);
+			cont += 2;
+		}
+		}
+		else
+		{
+			cont += _putchar(*format)
+		}
+			format++;
+		}
+		va_end(print);
+		return (cont);
+}
